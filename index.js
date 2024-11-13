def8fcd9f564be50283e14a57b8c0549870ec826
Vishwa Badrinadh Padala <vishwabadrinadh.padala@gmail.com>
2024-11-13 12:19:11 +0530

    backend

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/fruits-vegetables', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 
