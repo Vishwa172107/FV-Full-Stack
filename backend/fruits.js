0282123f007f2409354ba7a73bea4f9535d95abc
Vishwa Badrinadh Padala <vishwabadrinadh.padala@gmail.com>
2024-11-13 12:27:50 +0530

    backend

--- backend/fruits.js
+++ backend/fruits.js
@@ -23,16 +23,16 @@ const fruitsSchema = new mongoose.Schema({
 });
 const fruit = mongoose.model("fruit", fruitsSchema);
 
-fruit.insertMany([
-  {  name: 'Apple', img: 'https://images.pexels.com/photos/2487443/pexels-photo-2487443.jpeg?cs=srgb&dl=red-apples-2487443.jpg&fm=jpg', description: 'A sweet, crisp fruit that is high in fiber and vitamins.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
-  {  name: 'Banana', img: 'https://www.nipponexpress.com/press/report/img/06-Nov-20-ogp.jpeg', description: 'A potassium-rich fruit that is great for energy.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
-  {  name: 'Pineapple', img: 'https://www.oahufresh.com/wp-content/uploads/2012/05/pineapple.jpg', description: 'A fruit that is high in fiber and vitamins.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
-  {  name: 'Orange', img: 'https://tse4.mm.bing.net/th?id=OIP.VbKz6SeSX9CQifygrVttcwHaE7&pid=Api&P=0&h=180', description: 'A citrus fruit known for its high vitamin C content.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} `},
-  {  name: 'Pomegranate', img: 'https://idsb.tmgrup.com.tr/ly/uploads/images/2020/09/30/61638.jpg', description: 'A fruit known for its high vitamin C content.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
-  {  name: 'Guava', img: 'https://post.greatist.com/wp-content/uploads/2020/05/guava-1200x628-facebook-1200x628.jpg', description: 'A fruit rich in vitamin C.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
-  {  name: 'Strawberry', img: 'https://tse3.mm.bing.net/th?id=OIP.ESvtsBnYxWO40A061MtJoAHaEo&pid=Api&P=0&h=180', description: 'Fresh Strawberry', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
-  {  name: 'Watermelon', img: 'https://www.thespruce.com/thmb/pjDqYUU4EG1y_fNU4x23WCbqLXA=/4238x2827/filters:fill(auto,1)/when-to-harvest-watermelon-2539591-04-f7f99d3a73d846cb9d453f8a6f0cc7ee.jpg', description: 'Organic Watermelon', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` }
-]);
+// fruit.insertMany([
+//   {  name: 'Apple', img: 'https://images.pexels.com/photos/2487443/pexels-photo-2487443.jpeg?cs=srgb&dl=red-apples-2487443.jpg&fm=jpg', description: 'A sweet, crisp fruit that is high in fiber and vitamins.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
+//   {  name: 'Banana', img: 'https://www.nipponexpress.com/press/report/img/06-Nov-20-ogp.jpeg', description: 'A potassium-rich fruit that is great for energy.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
+//   {  name: 'Pineapple', img: 'https://www.oahufresh.com/wp-content/uploads/2012/05/pineapple.jpg', description: 'A fruit that is high in fiber and vitamins.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
+//   {  name: 'Orange', img: 'https://tse4.mm.bing.net/th?id=OIP.VbKz6SeSX9CQifygrVttcwHaE7&pid=Api&P=0&h=180', description: 'A citrus fruit known for its high vitamin C content.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} `},
+//   {  name: 'Pomegranate', img: 'https://idsb.tmgrup.com.tr/ly/uploads/images/2020/09/30/61638.jpg', description: 'A fruit known for its high vitamin C content.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
+//   {  name: 'Guava', img: 'https://post.greatist.com/wp-content/uploads/2020/05/guava-1200x628-facebook-1200x628.jpg', description: 'A fruit rich in vitamin C.', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
+//   {  name: 'Strawberry', img: 'https://tse3.mm.bing.net/th?id=OIP.ESvtsBnYxWO40A061MtJoAHaEo&pid=Api&P=0&h=180', description: 'Fresh Strawberry', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` },
+//   {  name: 'Watermelon', img: 'https://www.thespruce.com/thmb/pjDqYUU4EG1y_fNU4x23WCbqLXA=/4238x2827/filters:fill(auto,1)/when-to-harvest-watermelon-2539591-04-f7f99d3a73d846cb9d453f8a6f0cc7ee.jpg', description: 'Organic Watermelon', price: `₹${(Math.random() * 100 + 10).toFixed(2)} ` }
+// ]);
 
 
 module.exports = fruit;
