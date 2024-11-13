244282a8d7811d9f4bd894ec98959685efbdd5f6
Vishwa Badrinadh Padala <vishwabadrinadh.padala@gmail.com>
2024-11-13 12:11:41 +0530

    mongo

--- backend/fruits.js
+++ backend/fruits.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/fruits-vegetables');
+  const mongo_string = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fruits-vegetables'
+  await mongoose.connect(mongo_string);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
